b3fa1ea43c22d3159e0effb26735def65bdc50b7
Aditya pal <studymailaditya@gmail.com>
2025-05-10 20:50:40 +0530

    na

--- BASIC/src/STRING/string.java
+++ BASIC/src/STRING/string.java
@@ -1,5 +1,17 @@
 package STRING;
 
 public class string {
-    
+//     defination: there are three wat to create a string
+// 1st : new keyword , 2nd : String litrel 3rd: by converting character array into string
+public static void main(String[] args) {
+//    by using String leteral
+    String name="Aditya shreeram pal";
+
+//    BY NEW KEYWORD
+
+    String namm=new String("Aditya shreeram pal");
+
+//
 }
+}
+
